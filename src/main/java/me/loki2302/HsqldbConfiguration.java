2bf58d1b85830907cf27c8cbc112a139a6c24999
loki2302 <loki2302@loki2302.me>
2013-10-07 11:26:46 +0400

    Cleaning up

--- src/main/java/me/loki2302/HsqldbConfiguration.java
+++ src/main/java/me/loki2302/HsqldbConfiguration.java
@@ -1,55 +1,28 @@
 package me.loki2302;
 
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
 import me.loki2302.repositories.UserRepository;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 @EnableTransactionManagement
-public class HsqldbConfiguration {
-    @Bean
+public class HsqldbConfiguration extends AbstractConfiguration {
+    @Override
     public DataSource dataSource() {
         return new EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.HSQL)
             .build();
     }
-        
-    @Bean
-    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
-        return transactionManager;
-    }
-    
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        Properties jpaProperties = new Properties();
-        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.HSQLDialect");
-        jpaProperties.put("hibernate.hbm2ddl.auto", "create");
-        
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        
-        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(dataSource);
-        entityManagerFactory.setPackagesToScan(new String[] { "me.loki2302" });
-        entityManagerFactory.setPersistenceUnitName("MyPersistenceUnit");
-        entityManagerFactory.setJpaVendorAdapter(hibernateJpaVendorAdapter);        
-        entityManagerFactory.setJpaProperties(jpaProperties);
-        entityManagerFactory.afterPropertiesSet();
-        return entityManagerFactory;
-    }       
+
+    @Override
+    protected String getDialect() {
+        return "org.hibernate.dialect.HSQLDialect";
+    }           
 }
--- src/main/java/me/loki2302/PostgreSqlConfiguration.java
+++ src/main/java/me/loki2302/PostgreSqlConfiguration.java
@@ -1,8 +1,6 @@
 package me.loki2302;
 
 import java.sql.Driver;
-import java.util.Properties;
-
 import javax.sql.DataSource;
 
 import me.loki2302.repositories.UserRepository;
@@ -13,19 +11,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 @EnableTransactionManagement
-public class PostgreSqlConfiguration {    
+public class PostgreSqlConfiguration extends AbstractConfiguration {    
     private final static String DATABASE_NAME = "javatestdb";
     
     @Bean
+    @Override
     @SuppressWarnings("unchecked")
     public DataSource dataSource() throws ClassNotFoundException {
         Class<? extends Driver> postgreSqlDriver = 
@@ -63,29 +58,9 @@ public class PostgreSqlConfiguration {
         dataSource.setUrl(String.format("jdbc:postgresql://win7dev-home/%s?user=postgres&password=qwerty", DATABASE_NAME));
         return dataSource;
     }
-    
-    @Bean
-    public PlatformTransactionManager transactionManager(LocalContainerEntityManagerFactoryBean entityManagerFactory) {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory.getObject());
-        return transactionManager;
-    }
-    
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
-        Properties jpaProperties = new Properties();        
-        jpaProperties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect");
-        jpaProperties.put("hibernate.hbm2ddl.auto", "create");
-        
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        
-        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
-        entityManagerFactory.setDataSource(dataSource);
-        entityManagerFactory.setPackagesToScan(new String[] { "me.loki2302" });
-        entityManagerFactory.setPersistenceUnitName("MyPersistenceUnit");
-        entityManagerFactory.setJpaVendorAdapter(hibernateJpaVendorAdapter);        
-        entityManagerFactory.setJpaProperties(jpaProperties);
-        entityManagerFactory.afterPropertiesSet();
-        return entityManagerFactory;
+
+    @Override
+    protected String getDialect() {
+        return "org.hibernate.dialect.PostgreSQL82Dialect";
     }       
 }
