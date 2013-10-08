f3c94f71d0827d4f1b73f68f44f041a6b764db66
loki2302 <loki2302@loki2302.me>
2013-10-08 09:45:19 +0400

    Minor progress

--- src/main/java/me/loki2302/AuthenticationResult.java
+++ src/main/java/me/loki2302/AuthenticationResult.java
@@ -1,5 +1,7 @@
 package me.loki2302;
 
 public class AuthenticationResult {
+    public String Provider;
+    public String Message;
     public String UserDetailsJson;
 }
--- src/main/java/me/loki2302/HomeController.java
+++ src/main/java/me/loki2302/HomeController.java
@@ -3,6 +3,7 @@ package me.loki2302;
 import java.io.IOException;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 import org.codehaus.jackson.annotate.JsonProperty;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -21,6 +22,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -28,8 +30,10 @@ import org.springframework.web.servlet.view.RedirectView;
 public class HomeController {
     private final OAuthService googleService = new ServiceBuilder()
         .provider(GoogleApi.class)
-        .apiKey("TODO")
-        .apiSecret("TODO")
+        .apiKey("330741531920.apps.googleusercontent.com")
+        .apiSecret("R21tppN-oV9bAqg-Sgp5tTNg")
+        .scope("https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email")
+        .callback("http://localhost:8080/googleCallback")        
         .build();
     
     private final OAuthService facebookService = new ServiceBuilder()
@@ -42,10 +46,10 @@ public class HomeController {
     
     private final OAuthService twitterService = new ServiceBuilder()
         .provider(TwitterApi.class)
-        .apiKey("TODO")
-        .apiSecret("TODO")
-        .build();
-    
+        .apiKey("JRBEmQEBhV1B5gJOvzA3ag")
+        .apiSecret("VlB7e9wA0WIRmVOVzGpLQqgzUjAyklezXdWCXTSM21Y")
+        .callback("http://localhost:8080/twitterCallback")
+        .build();    
     
     @RequestMapping(value = "/", method = RequestMethod.GET)    
     public String index() {
@@ -54,7 +58,39 @@ public class HomeController {
     
     @RequestMapping(value = "/google", method = RequestMethod.GET)
     public View authenticateWithGoogle() {
-        throw new RuntimeException();
+        Token requestToken = googleService.getRequestToken();
+        String authorizationUrl = googleService.getAuthorizationUrl(requestToken);
+        return new RedirectView(authorizationUrl);
+    }
+    
+    @RequestMapping(value = "/googleCallback", method = RequestMethod.GET)
+    public String googleCallback(
+            @RequestParam(value = "oauth_token", required = false) String oauthToken, 
+            @RequestParam(value = "oauth_verifier", required = false) String oauthVerifier,
+            Model model) throws JsonParseException, JsonMappingException, IOException {
+        
+        AuthenticationResult result = new AuthenticationResult();
+        result.Provider = "Google";
+        
+        Token requestToken = new Token(oauthToken, oauthVerifier);
+        Verifier verifier = new Verifier(oauthVerifier);
+        Token accessToken = googleService.getAccessToken(requestToken, verifier);
+        
+        OAuthRequest request = new OAuthRequest(Verb.GET, "https://www.googleapis.com/oauth2/v1/userinfo");
+        googleService.signRequest(accessToken, request);
+        Response response = request.send();
+        System.out.println(response.getBody());
+        
+        ObjectMapper mapper = new ObjectMapper();
+        GoogleMe googleUser = mapper.readValue(response.getBody(), GoogleMe.class);
+        
+        result.Message = "OK";
+        result.UserDetailsJson = googleUser.toString();
+        
+        return "index";
+    }
+    
+    public static class GoogleMe {        
     }
     
     @RequestMapping(value = "/facebook", method = RequestMethod.GET)
@@ -64,22 +100,35 @@ public class HomeController {
     }
     
     @RequestMapping(value = "/facebookCallback", method = RequestMethod.GET)
-    public String facebookCallback(String code, Model model) throws JsonParseException, JsonMappingException, IOException {        
-        Verifier verifier = new Verifier(code);
-        Token accessToken = facebookService.getAccessToken(null, verifier);                
-        
-        OAuthRequest request = new OAuthRequest(Verb.GET, "https://graph.facebook.com/me");
-        facebookService.signRequest(accessToken, request);
-        Response response = request.send();
-        System.out.printf("Response (%d): %s\n", response.getCode(), response.getBody());
-        
-        ObjectMapper mapper = new ObjectMapper();
-        FacebookMe u = mapper.readValue(response.getBody(), FacebookMe.class);
-        
-        System.out.println(u);
+    public String facebookCallback(
+            @RequestParam(value = "code", required = false) String code, 
+            @RequestParam(value = "error", required = false) String error, 
+            Model model) throws JsonParseException, JsonMappingException, IOException {
         
         AuthenticationResult result = new AuthenticationResult();
-        result.UserDetailsJson = u.toString();
+        result.Provider = "Facebook";
+        
+        if(error != null) {
+            if(error.equals("access_denied")) {
+                result.Message = "User cancelled";
+            } else {
+                result.Message = String.format("Something bad happened: %s", error);                        
+            }
+        } else {        
+            Verifier verifier = new Verifier(code);
+            Token accessToken = facebookService.getAccessToken(null, verifier);                
+            
+            OAuthRequest request = new OAuthRequest(Verb.GET, "https://graph.facebook.com/me");
+            facebookService.signRequest(accessToken, request);
+            Response response = request.send();
+            
+            ObjectMapper mapper = new ObjectMapper();
+            FacebookMe facebookUser = mapper.readValue(response.getBody(), FacebookMe.class);                        
+            
+            result.Message = "OK";
+            result.UserDetailsJson = facebookUser.toString();            
+        }
+        
         model.addAttribute("result", result);
         
         return "index";
@@ -87,9 +136,45 @@ public class HomeController {
     
     @RequestMapping(value = "/twitter", method = RequestMethod.GET)
     public View authenticateWithTwitter() {
-        throw new RuntimeException();
+        Token requestToken = twitterService.getRequestToken();
+        String authorizationUrl = twitterService.getAuthorizationUrl(requestToken);
+        return new RedirectView(authorizationUrl);
     }
     
+    @RequestMapping(value = "/twitterCallback", method = RequestMethod.GET)
+    public String twitterCallback(
+            @RequestParam(value = "oauth_token", required = false) String oauthToken, 
+            @RequestParam(value = "oauth_verifier", required = false) String oauthVerifier,
+            @RequestParam(value = "denied", required = false) String denied,
+            Model model) throws JsonParseException, JsonMappingException, IOException {
+        
+        AuthenticationResult result = new AuthenticationResult();
+        result.Provider = "Twitter";
+        
+        if(denied != null && !denied.equals("")) {
+            result.Message = "User cancelled";
+        } else {                
+            Token requestToken = new Token(oauthToken, oauthVerifier);
+            Verifier verifier = new Verifier(oauthVerifier);
+            Token accessToken = twitterService.getAccessToken(requestToken, verifier);
+            
+            OAuthRequest request = new OAuthRequest(Verb.GET, "http://api.twitter.com/1.1/account/verify_credentials.json");
+            twitterService.signRequest(accessToken, request);
+            Response response = request.send();
+            
+            ObjectMapper mapper = new ObjectMapper();
+            TwitterMe twitterUser = mapper.readValue(response.getBody(), TwitterMe.class);
+            
+            result.Message = "OK";
+            result.UserDetailsJson = twitterUser.toString();
+        }
+        
+        model.addAttribute("result", result);
+        
+        return "index";        
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
     public static class FacebookMe {
         @JsonProperty("id") public String Id;
         @JsonProperty("name") public String Name;
@@ -106,9 +191,35 @@ public class HomeController {
                 
         @Override
         public String toString() {
-            ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
+            return JsonUtils.asJson(this);
+        }
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class TwitterMe {
+        @JsonProperty("id") public String Id;
+        @JsonProperty("name") public String Name;
+        @JsonProperty("screen_name") public String ScreenName;
+        @JsonProperty("url") public String Url;
+        @JsonProperty("followers_count") public int FollowersCount;
+        @JsonProperty("friends_count") public int FriendsCount;
+        @JsonProperty("time_zone") public String TimeZone;
+        @JsonProperty("verified") public boolean Verified;
+        @JsonProperty("profile_image_url") public String ProfileImageUrl;
+
+        @Override
+        public String toString()
+        {
+            return JsonUtils.asJson(this);
+        }
+    }    
+    
+    public static class JsonUtils {
+        private final static ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();
+        
+        public static String asJson(Object o) {
             try {
-                return objectWriter.writeValueAsString(this);
+                return objectWriter.writeValueAsString(o);
             } catch (JsonGenerationException e) {
                 return "FAIL";
             } catch (JsonMappingException e) {
