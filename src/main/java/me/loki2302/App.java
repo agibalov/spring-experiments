29b59123d51161cca000e40c4c673f794227a8c7
loki2302 <loki2302@loki2302.me>
2014-05-14 13:07:23 -0700

    Page 2 now displays a collection of items

--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) {
@@ -29,7 +31,24 @@ public class App {
 
         @RequestMapping("/page2")
         public String page2(Model model) {
+            List<Person> people = new ArrayList<Person>();
+            for(int i = 0; i < 10; ++i) {
+                people.add(new Person(i + 1, String.format("John #%d", i + 1)));
+            }
+
+            model.addAttribute("people", people);
+
             return "page2";
+        }
+    }
+
+    public static class Person {
+        public int id;
+        public String name;
+
+        public Person(int id, String name) {
+            this.id = id;
+            this.name = name;
         }
     }
 }
