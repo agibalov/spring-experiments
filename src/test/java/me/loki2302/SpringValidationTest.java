c85a29c6d7fd32797a10890a05abe8e5127f1aff
loki2302 <loki2302@loki2302.me>
2014-05-31 06:13:12 -0700

    Added Spring Validation test

--- src/test/java/me/loki2302/SpringValidationTest.java
+++ src/test/java/me/loki2302/SpringValidationTest.java
@@ -16,24 +16,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
-
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @IntegrationTest
-@SpringApplicationConfiguration(classes = BeanValidationTest.Config.class)
+@SpringApplicationConfiguration(classes = SpringValidationTest.Config.class)
 @WebAppConfiguration
 @DirtiesContext
-public class BeanValidationTest {
+public class SpringValidationTest {
     @Autowired
     private RestTemplate restTemplate;
 
@@ -69,8 +72,8 @@ public class BeanValidationTest {
     @EnableAutoConfiguration
     public static class Config {
         @Bean
-        public LocalValidatorFactoryBean validator() {
-            return new LocalValidatorFactoryBean();
+        public UserNameDtoValidator userNameDtoValidator() {
+            return new UserNameDtoValidator();
         }
 
         @Bean
@@ -88,6 +91,14 @@ public class BeanValidationTest {
 
     @Controller
     public static class HomeController {
+        @Autowired
+        private UserNameDtoValidator userNameDtoValidator;
+
+        @InitBinder
+        private void initBinder(WebDataBinder webDataBinder) {
+            webDataBinder.addValidators(userNameDtoValidator);
+        }
+
         @RequestMapping(value = "/", method = RequestMethod.POST)
         @ResponseBody
         @ResponseStatus(HttpStatus.OK)
@@ -104,8 +115,28 @@ public class BeanValidationTest {
     }
 
     public static class UserNameDto {
-        @NotEmpty
-        public String username;
+        private String username;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+    }
+
+    public static class UserNameDtoValidator implements Validator {
+        @Override
+        public boolean supports(Class<?> clazz) {
+            return clazz.equals(UserNameDto.class);
+        }
+
+        @Override
+        public void validate(Object target, Errors errors) {
+            UserNameDto userNameDto = (UserNameDto)target;
+            ValidationUtils.rejectIfEmpty(errors, "username", "required");
+        }
     }
 
     private static class NonThrowingResponseErrorHandler implements ResponseErrorHandler {
