81fb3c27cd0ef6144b96285999e43bd64fd797bf
loki2302 <loki2302@loki2302.me>
2014-11-10 09:10:51 +0300

    Converted to Spring Boot app

--- src/main/java/me/loki2302/AppConfiguration.java
+++ src/main/java/me/loki2302/AppConfiguration.java
@@ -1,41 +1,60 @@
 package me.loki2302;
 
-import java.sql.Driver;
-
-import javax.sql.DataSource;
-
+import com.googlecode.flyway.core.Flyway;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import com.googlecode.flyway.core.Flyway;
+import javax.sql.DataSource;
+import java.sql.Driver;
+import java.util.List;
 
 @Configuration
-public class DataConfiguration {   
+@ComponentScan
+@EnableAutoConfiguration
+public class AppConfiguration extends WebMvcConfigurerAdapter {
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        //multipartResolver.setMaxUploadSize(1024 * 1024);        
+        return multipartResolver;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.add(new DownloadableFileMessageConverter());
+    }
+
     @Bean
     @SuppressWarnings("unchecked")
-    public DataSource dataSource() throws ClassNotFoundException {        
+    public DataSource dataSource() throws ClassNotFoundException {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
         dataSource.setDriverClass((Class<? extends Driver>)Class.forName("org.hsqldb.jdbcDriver"));
         dataSource.setUrl("jdbc:hsqldb:mem:.");
         dataSource.setUsername("sa");
         dataSource.setPassword("");
-        
+
         Flyway flyway = new Flyway();
         flyway.setDataSource(dataSource);
         flyway.migrate();
-        
+
         return dataSource;
     }
-    
+
     @Bean
     public PlatformTransactionManager transactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
-        
+
     @Bean
     public NamedParameterJdbcTemplate jdbcTemplate(DataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
