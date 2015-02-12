34595e6cb50de8f6ed5d0fe0878c17fa163f2a4d
loki2302 <loki2302@loki2302.me>
2015-02-12 22:56:06 +0300

    Added an 31337 banner

--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -1,13 +1,21 @@
 package me.loki2302;
 
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.core.io.ClassPathResource;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication app = new SpringApplicationBuilder()
+                .sources(App.class)
+                .banner(new ResourceBanner(new ClassPathResource("/banner.txt")))
+                .build();
+
+        app.run(args);
     }
 }
