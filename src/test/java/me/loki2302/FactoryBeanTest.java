a69d7b3b8f2369852a95dd7e1c9d0ed967ad1d91
loki2302 <loki2302@loki2302.me>
2015-06-07 11:00:36 +0300

    Added FactoryBeanTest

---
+++ src/test/java/me/loki2302/FactoryBeanTest.java
@@ -0,0 +1,68 @@
+package me.loki2302;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AbstractFactoryBean;
+import org.springframework.boot.test.IntegrationTest;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@IntegrationTest
+@SpringApplicationConfiguration(classes = FactoryBeanTest.Config.class)
+public class FactoryBeanTest {
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Test
+    public void calculatorFactoryBeanConstructsACalculator() {
+        Calculator calculator = applicationContext.getBean(Calculator.class);
+        assertEquals(5, calculator.addNumbers(2, 3));
+    }
+
+    @Configuration
+    public static class Config {
+        @Bean
+        public CalculatorFactoryBean calculatorFactoryBean() {
+            return new CalculatorFactoryBean();
+        }
+    }
+
+    public static class CalculatorFactoryBean extends AbstractFactoryBean<Calculator> {
+        @Override
+        public Class<?> getObjectType() {
+            return Calculator.class;
+        }
+
+        @Override
+        protected Calculator createInstance() throws Exception {
+            AddNumbersHandler addNumbersHandler = new AddNumbersHandler();
+            Calculator calculator = new Calculator(addNumbersHandler);
+            return calculator;
+        }
+    }
+
+    public static class Calculator {
+        private final AddNumbersHandler addNumbersHandler;
+
+        public Calculator(AddNumbersHandler addNumbersHandler) {
+            this.addNumbersHandler = addNumbersHandler;
+        }
+
+        public int addNumbers(int a, int b) {
+            return a + b;
+        }
+    }
+
+    public static class AddNumbersHandler {
+        public int addNumbers(int a, int b) {
+            return a + b;
+        }
+    }
+}
