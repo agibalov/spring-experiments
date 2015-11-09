41080c548ee9fe4b4be37ab582f7c1e4f94f062f
loki2302 <loki2302@loki2302.me>
2015-11-09 23:00:08 +0300

    Updated dependencies

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 spring-swagger-experiment
 =========================
 
-1. `gradlew clean run`, then go to http://http://localhost:8080/swagger-ui.html
-2. See there's a list box with /api-docs and /v2/api-docs
+`gradlew clean run`, then go to http://http://localhost:8080/swagger-ui.html
+
--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -13,7 +13,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger1.annotations.EnableSwagger;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static com.google.common.base.Predicates.or;
@@ -25,15 +24,15 @@ public class App {
     }
 
     @Configuration
-    @EnableSwagger
     @EnableSwagger2
     @ComponentScan
     @EnableAutoConfiguration
     public static class Config {
         @Bean
         public Docket api() {
-            return new Docket(DocumentationType.SWAGGER_12)
+            return new Docket(DocumentationType.SWAGGER_2)
                     .apiInfo(apiInfo())
+                    .useDefaultResponseMessages(false)
                     .select()
                     .paths(or(ant("/api/*"), ant("/api/**/*")))
                     .build();
