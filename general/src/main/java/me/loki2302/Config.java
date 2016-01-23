080f4ed5a367456101f339185cfe379d88fd1d0d
loki2302 <loki2302@loki2302.me>
2016-01-23 12:50:22 +0300

    Updated Spring Security to 1.3.2.RELEASE

--- general/src/main/java/me/loki2302/Config.java
+++ general/src/main/java/me/loki2302/Config.java
@@ -1,18 +1,31 @@
 package me.loki2302;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @Configuration
-@ComponentScan
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class Config {
     @Bean
     AuthenticationManager authenticationManager(AuthenticationManagerBuilder builder) {
         return builder.getOrBuild();
+    }
+
+    @Bean
+    public Calculator calculator() {
+        return new Calculator();
+    }
+
+    @Bean
+    public DivSecurityDecider divSecurityDecider() {
+        return new DivSecurityDecider();
+    }
+
+    @Bean
+    public NoteService noteService() {
+        return new NoteService();
     }
 }
