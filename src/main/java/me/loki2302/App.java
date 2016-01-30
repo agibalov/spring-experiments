8d262e83dbd83f3dcb79c4b2f6ab51521834a91f
loki2302 <loki2302@loki2302.me>
2016-01-30 08:38:42 +0300

    Updated dependencies

--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -1,8 +1,7 @@
 package me.loki2302;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +10,10 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(Config.class, args);
-    }
-
-    @ComponentScan
-    @EnableAutoConfiguration
-    public static class Config {
+        SpringApplication.run(App.class, args);
     }
 
     @Controller
