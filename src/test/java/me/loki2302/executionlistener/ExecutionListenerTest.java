ae0973ee4df9665d65039b2f4b86f33da81da70e
loki2302 <loki2302@loki2302.me>
2016-10-21 21:34:57 -0400

    Added ExecutionListenerTest

---
+++ src/test/java/me/loki2302/executionlistener/ExecutionListenerTest.java
@@ -0,0 +1,62 @@
+package me.loki2302.executionlistener;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+
+import java.lang.reflect.Method;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@TestExecutionListeners(
+        listeners = ExecutionListenerTest.TestNameUpdatingTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS)
+public class ExecutionListenerTest {
+    @Autowired
+    private TestNameHolder testNameHolder;
+
+    @Test
+    public void testNameHolderShouldHaveNameSetBeforeTestStart() {
+        String expectedName = String.format("%s %s %s",
+                ExecutionListenerTest.class.getName(),
+                "testNameHolderShouldHaveNameSetBeforeTestStart",
+                this);
+
+        assertEquals(expectedName, testNameHolder.name);
+    }
+
+    public static class TestNameUpdatingTestExecutionListener extends AbstractTestExecutionListener {
+        @Override
+        public void beforeTestMethod(TestContext testContext) throws Exception {
+            Class<?> testClass = testContext.getTestClass();
+            Method testMethod = testContext.getTestMethod();
+            Object testInstance = testContext.getTestInstance();
+
+            ApplicationContext applicationContext = testContext.getApplicationContext();
+            TestNameHolder testNameHolder = applicationContext.getBean(TestNameHolder.class);
+            testNameHolder.name = String.format("%s %s %s", testClass.getName(), testMethod.getName(), testInstance);
+        }
+    }
+
+    @Configuration
+    public static class Config {
+        @Bean
+        public TestNameHolder testNameHolder() {
+            return new TestNameHolder();
+        }
+    }
+
+    public static class TestNameHolder {
+        public String name;
+    }
+}
