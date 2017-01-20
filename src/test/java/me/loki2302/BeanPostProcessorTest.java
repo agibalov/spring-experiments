afba0ad619fb649b1b6f3e02b3b362f77ada0e0f
loki2302 <loki2302@loki2302.me>
2017-01-19 22:06:42 -0500

    Progress with BeanPostProcessorTest

--- src/test/java/me/loki2302/BeanPostProcessorTest.java
+++ src/test/java/me/loki2302/BeanPostProcessorTest.java
@@ -12,19 +12,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -37,8 +32,11 @@ public class BeanPostProcessorTest {
 
     @Test
     public void canConfigureABeanBasedOnOtherBeans() {
-        assertTrue(theHandlerRepository.getHandlerForRequestType(AddNumbersRequest.class) instanceof AddNumbersHandler);
-        assertTrue(theHandlerRepository.getHandlerForRequestType(SubNumbersRequest.class) instanceof SubNumbersHandler);
+        Integer sum = theHandlerRepository.<AddNumbersRequest, Integer>handle(new AddNumbersRequest(2, 3));
+        assertEquals(5, (int) sum);
+
+        Integer difference = theHandlerRepository.<SubNumbersRequest, Integer>handle(new SubNumbersRequest(2, 3));
+        assertEquals(-1, (int) difference);
     }
 
     @Configuration
@@ -73,13 +71,10 @@ public class BeanPostProcessorTest {
         @Override
         public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
             if(bean instanceof TheHandlerRepository) {
-                Map<String, Object> handlers = listableBeanFactory.getBeansWithAnnotation(TheHandler.class);
-
-                Map<Class<?>, Object> handlersByRequestTypes = handlers.values().stream()
+                Map<String, TheHandler> handlers = listableBeanFactory.getBeansOfType(TheHandler.class);
+                Map<Class<?>, TheHandler> handlersByRequestTypes = handlers.values().stream()
                         .collect(Collectors.toMap(
-                                handler -> AnnotationUtils.getAnnotation(
-                                        handler.getClass(),
-                                        TheHandler.class).value(),
+                                handler -> handler.getRequestClass(),
                                 handler -> handler));
 
                 LOGGER.info("Handlers: {}", handlersByRequestTypes);
@@ -97,30 +92,66 @@ public class BeanPostProcessorTest {
     }
 
     public static class TheHandlerRepository {
-        private Map<Class<?>, Object> handlersByRequestTypes = new HashMap<>();
+        private Map<Class<?>, TheHandler> handlersByRequestTypes = new HashMap<>();
 
-        public void add(Map<Class<?>, Object> handlersByRequestTypes) {
+        public void add(Map<Class<?>, TheHandler> handlersByRequestTypes) {
             this.handlersByRequestTypes.putAll(handlersByRequestTypes);
         }
 
-        public Object getHandlerForRequestType(Class<?> requestTypeClass) {
-            return handlersByRequestTypes.get(requestTypeClass);
+        public <TRequest, TResponse> TResponse handle(TRequest request) {
+            TheHandler theHandler = handlersByRequestTypes.get(request.getClass());
+            return (TResponse)theHandler.handle(request);
+        }
+    }
+
+    public interface TheHandler<TRequest, TResponse> {
+        Class<TRequest> getRequestClass();
+        TResponse handle(TRequest request);
+    }
+
+    @Component
+    public static class AddNumbersHandler implements TheHandler<AddNumbersRequest, Integer> {
+        @Override
+        public Class<AddNumbersRequest> getRequestClass() {
+            return AddNumbersRequest.class;
+        }
+
+        @Override
+        public Integer handle(AddNumbersRequest addNumbersRequest) {
+            return addNumbersRequest.a + addNumbersRequest.b;
         }
     }
 
     @Component
-    @Target(ElementType.TYPE)
-    @Retention(RetentionPolicy.RUNTIME)
-    public @interface TheHandler {
-        Class<?> value();
+    public static class SubNumbersHandler implements TheHandler<SubNumbersRequest, Integer> {
+        @Override
+        public Class<SubNumbersRequest> getRequestClass() {
+            return SubNumbersRequest.class;
+        }
+
+        @Override
+        public Integer handle(SubNumbersRequest subNumbersRequest) {
+            return subNumbersRequest.a - subNumbersRequest.b;
+        }
     }
 
-    @TheHandler(AddNumbersRequest.class)
-    public static class AddNumbersHandler {}
+    public static class AddNumbersRequest {
+        public final int a;
+        public final int b;
 
-    @TheHandler(SubNumbersRequest.class)
-    public static class SubNumbersHandler {}
+        public AddNumbersRequest(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
 
-    public static class AddNumbersRequest {}
-    public static class SubNumbersRequest {}
+    public static class SubNumbersRequest {
+        public final int a;
+        public final int b;
+
+        public SubNumbersRequest(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
 }
