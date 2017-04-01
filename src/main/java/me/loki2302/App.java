a8b4dda19771b5654344cae230720ea26703a93e
loki2302 <loki2302@loki2302.me>
2017-03-31 21:55:34 -0400

    Added CounterAndGougeDemoService

--- src/main/java/me/loki2302/App.java
+++ src/main/java/me/loki2302/App.java
@@ -1,12 +1,15 @@
 package me.loki2302;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.health.AbstractHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.info.Info;
 import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.actuate.info.MapInfoContributor;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Component;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 @SpringBootApplication
 @EnableAdminServer
@@ -45,6 +49,28 @@ public class App {
             builder
                     .withDetail("Up/Down count", upDownCount)
                     .withDetail("Current time", new Date().toString());
+        }
+    }
+
+    @Component
+    public static class CounterAndGougeDemoService {
+        private final Random random = new Random();
+
+        @Autowired
+        private CounterService counterService;
+
+        @Autowired
+        private GaugeService gaugeService;
+
+        @Scheduled(fixedRate = 1000)
+        public void update() {
+            // SBA displays this in gauges
+            gaugeService.submit("dummy", 100 + random.nextInt(30)); // gauge.servo.dummy
+
+            // SBA displays this in counters
+            // IMPORTANT: the name should be counter.xxxx
+            // or otherwise SBA will display it in gauges, not in counters
+            counterService.increment("counter.dummy"); // gauge.servo.counter.dummy
         }
     }
 
