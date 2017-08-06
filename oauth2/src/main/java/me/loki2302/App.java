40f3eb47db785fac20b98df8bf9d49cc01c8e658
loki2302 <loki2302@loki2302.me>
2017-08-06 00:35:09 -0400

    Added OAuth2 experiment

---
+++ oauth2/src/main/java/me/loki2302/App.java
@@ -0,0 +1,125 @@
+package me.loki2302;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @RestController
+    public static class HomeControlle {
+        @GetMapping("/")
+        public Map<String, String> hello(Principal principal) throws IOException {
+            String responseText;
+            if(principal == null) {
+                responseText = String.format("PRINCIPAL is null");
+            } else if(principal instanceof OAuth2Authentication) {
+                OAuth2Authentication oAuth2Authentication = (OAuth2Authentication)principal;
+                Object innerPrincipal = oAuth2Authentication.getPrincipal();
+                if(innerPrincipal instanceof String) {
+                    String clientId = (String)innerPrincipal;
+                    responseText = String.format("PRINCIPAL is client '%s' (%s)", clientId, oAuth2Authentication.getAuthorities());
+                } else if(innerPrincipal instanceof User) {
+                    User user = (User)innerPrincipal;
+                    responseText = String.format("PRINCIPAL is user '%s' (%s)", user.getUsername(), oAuth2Authentication.getAuthorities());
+                } else {
+                    responseText = String.format("PRINCIPAL is OAuth2??? (%s)", oAuth2Authentication.getAuthorities());
+                }
+            } else {
+                responseText = String.format("PRINCIPAL is %s???", principal.getClass());
+            }
+
+            return Collections.singletonMap("message", responseText);
+        }
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user1")
+                    .password("user1password")
+                    .roles("USER");
+        }
+
+        @Bean
+        @Override
+        public AuthenticationManager authenticationManagerBean() throws Exception {
+            return super.authenticationManagerBean();
+        }
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    public static class OAuth2AuthorizationServiceConfig extends AuthorizationServerConfigurerAdapter {
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients
+                    .inMemory()
+                    .withClient("MyClientId1")
+                    .secret("MyClientId1Secret")
+                    .authorizedGrantTypes("client_credentials", "password", "refresh_token", "authorization_code")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read");
+        }
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints
+                    .tokenStore(tokenStore())
+                    .authenticationManager(authenticationManager);
+        }
+
+        @Bean
+        public TokenStore tokenStore() {
+            return new InMemoryTokenStore();
+        }
+    }
+
+    @Configuration
+    @EnableResourceServer
+    public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+        @Override
+        public void configure(HttpSecurity http) throws Exception {
+            // remove this to disallow requests without authentication
+            http.authorizeRequests().anyRequest().permitAll();
+        }
+    }
+}
