7e712eefa7fe9f1c10159a1050d558691da253a8
loki2302 <loki2302@loki2302.me>
2017-08-27 19:14:16 -0400

    Added jcommander-experiment

---
+++ jcommander-experiment/src/main/java/me/loki2302/App.java
@@ -0,0 +1,143 @@
+package me.loki2302;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import com.beust.jcommander.Parameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(App.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
+    }
+
+    @Component
+    public static class MyCommandLineRunner implements CommandLineRunner {
+        private final static Logger LOGGER = LoggerFactory.getLogger(MyCommandLineRunner.class);
+
+        @Autowired
+        private CommonArguments commonArguments;
+
+        @Autowired
+        private ApplicationContext applicationContext;
+
+        @Override
+        public void run(String... args) throws Exception {
+            JCommander.Builder jCommanderBuilder = JCommander.newBuilder()
+                    .addObject(commonArguments);
+            applicationContext.getBeansWithAnnotation(Parameters.class)
+                    .forEach((k, v) -> jCommanderBuilder.addCommand(v));
+            JCommander jCommander = jCommanderBuilder
+                    .programName("tool")
+                    .build();
+
+            boolean hasError = false;
+            try {
+                jCommander.parse(args);
+            } catch (ParameterException e) {
+                System.out.printf("%s\n", e.getMessage());
+                hasError = true;
+            }
+
+            String command = jCommander.getParsedCommand();
+            if(command == null) {
+                jCommander.usage();
+                return;
+            } else if(hasError) {
+                jCommander.usage(command);
+                return;
+            }
+
+            Map<String, Runnable> commandHandlers = applicationContext.getBeansOfType(Runnable.class);
+
+            Optional<Runnable> commandHandlerOptional = commandHandlers.values().stream()
+                    .filter(h -> Arrays.binarySearch(h.getClass().getAnnotation(Parameters.class).commandNames(), command) >= 0)
+                    .findFirst();
+            if(!commandHandlerOptional.isPresent()) {
+                System.out.printf("There is no handler for command %s\n", command);
+                return;
+            }
+
+            Runnable commandHandler = commandHandlerOptional.get();
+            commandHandler.run();
+        }
+    }
+
+    @Component
+    public static class CommonArguments {
+        @Parameter(names = "--verbose", description = "Lots of console output")
+        public boolean verbose;
+    }
+
+    @Component
+    @Parameters(commandNames = "deploy", commandDescription = "Deploy the stuff")
+    public static class DeployArguments implements Runnable {
+        private final static Logger LOGGER = LoggerFactory.getLogger(DeployArguments.class);
+
+        @Parameter(names = "--what", description = "What needs to be deployed", required = true)
+        public String what;
+
+        @Autowired
+        private CommonArguments commonArguments;
+
+        @Autowired
+        private DeploymentService deploymentService;
+
+        @Override
+        public void run() {
+            LOGGER.info("Deploying {}", what);
+            LOGGER.info("Verbose: {}", commonArguments.verbose);
+            deploymentService.deploy();
+        }
+    }
+
+    @Component
+    @Parameters(commandNames = "undeploy", commandDescription = "Undeploy the stuff")
+    public static class UndeployArguments implements Runnable {
+        private final static Logger LOGGER = LoggerFactory.getLogger(UndeployArguments.class);
+
+        @Parameter(names = "--what", description = "What needs to be undeployed", required = true)
+        public String what;
+
+        @Autowired
+        private CommonArguments commonArguments;
+
+        @Autowired
+        private DeploymentService deploymentService;
+
+        @Override
+        public void run() {
+            LOGGER.info("Undeploying {}", what);
+            LOGGER.info("Verbose: {}", commonArguments.verbose);
+            deploymentService.undeploy();
+        }
+    }
+
+    @Component
+    public static class DeploymentService {
+        private final static Logger LOGGER = LoggerFactory.getLogger(DeploymentService.class);
+
+        public void deploy() {
+            LOGGER.info("Deploying...");
+        }
+
+        public void undeploy() {
+            LOGGER.info("Undeploying...");
+        }
+    }
+}
