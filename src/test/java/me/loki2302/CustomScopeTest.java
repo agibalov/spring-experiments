52b92e18eaa27b6eca2219b61747ec92c32c52b7
loki2302 <loki2302@loki2302.me>
2017-09-10 22:08:57 -0400

    Updated the CustomScopeTest

--- src/test/java/me/loki2302/CustomScopeTest.java
+++ src/test/java/me/loki2302/CustomScopeTest.java
@@ -2,18 +2,17 @@ package me.loki2302;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.core.annotation.AliasFor;
-import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.lang.annotation.Retention;
@@ -27,166 +26,171 @@ import static org.junit.Assert.assertEquals;
 @SpringBootTest
 public class CustomScopeTest {
     @Autowired
-    private SingletonScopedBean singletonScopedBean;
+    private MyOperationHandler myOperationHandler;
+
+    @Autowired
+    private OperationResponse operationResponse;
 
     @Test
-    public void shouldAllowMeToUseCustomScope() {
-        OperationContext operationContext1 = new OperationContext("operation1");
-        OperationContextHolder.setOperationContext(operationContext1);
-        assertEquals("operation1", singletonScopedBean.getOperationName());
+    public void dummy() {
+        OperationContext operationContext = new OperationContext();
+        operationContext.set("rawRequestContent", "hello");
+        OperationContextHolder.setOperationContext(operationContext);
 
-        OperationContext operationContext2 = new OperationContext("operation2");
-        OperationContextHolder.setOperationContext(operationContext2);
-        assertEquals("operation2", singletonScopedBean.getOperationName());
+        myOperationHandler.handleRequest();
 
-        OperationContextHolder.setOperationContext(operationContext1);
-        assertEquals("operation1", singletonScopedBean.getOperationName());
+        assertEquals("operation response for hello", operationResponse.getOutput());
+        OperationContextHolder.reset();
+    }
+
+    @Retention(RetentionPolicy.RUNTIME)
+    @Scope(OperationScopeImpl.SCOPE_NAME)
+    public @interface OperationScope {
+        @AliasFor(annotation = Scope.class)
+        ScopedProxyMode proxyMode() default ScopedProxyMode.DEFAULT;
     }
 
     @Configuration
     public static class Config {
         @Bean
-        public SingletonScopedBean singletonScopedBean() {
-            return new SingletonScopedBean();
+        public static OperationScopeBeanFactoryPostProcessor operationScopeBeanFactoryPostProcessor() {
+            return new OperationScopeBeanFactoryPostProcessor();
         }
 
         @Bean
-        @OperationScope
-        public OperationScopedBean operationScopedBean() {
-            return new OperationScopedBean();
+        @OperationScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+        public OperationRequest request() {
+            String input = (String)OperationContextHolder.getOperationContext().get("rawRequestContent");
+            return new OperationRequest(input);
         }
 
         @Bean
-        public BeanFactoryPostProcessor beanFactoryPostProcessor() {
-            return beanFactory ->
-                    beanFactory.registerScope(OperationScopeImpl.NAME, new OperationScopeImpl());
+        @OperationScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+        public OperationResponse response() {
+            return new OperationResponse();
+        }
+
+        @Bean
+        @OperationScope(proxyMode = ScopedProxyMode.TARGET_CLASS)
+        public MyOperationHandler myOperationHandler() {
+            return new MyOperationHandler();
         }
     }
 
-    public static class OperationContext {
-        private final String name;
-        private final Map<String, Object> attributes = new HashMap<>();
-
-        public OperationContext(String name) {
-            this.name = name;
-        }
-
-        public Object getAttribute(String name) {
-            return attributes.get(name);
-        }
-
-        public void setAttribute(String name, Object value) {
-            attributes.put(name, value);
-        }
-
-        public void removeAttribute(String name) {
-            attributes.remove(name);
+    public static class OperationScopeBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+            beanFactory.registerScope(OperationScopeImpl.SCOPE_NAME, new OperationScopeImpl());
         }
     }
 
-    public static class OperationContextHolder {
-        private static OperationContext OPERATION_CONTEXT;
-
-        public static void setOperationContext(OperationContext operationContext) {
-            OPERATION_CONTEXT = operationContext;
-        }
-
-        public static OperationContext getOperationContext() {
-            return OPERATION_CONTEXT;
-        }
-    }
-
-    public static class OperationScopedBean {
-        private String name;
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-    }
-
-    @Component
-    public static class SingletonScopedBean {
-        private final Logger LOGGER = LoggerFactory.getLogger(SingletonScopedBean.class);
+    public static class MyOperationHandler {
+        @Autowired
+        private OperationRequest operationRequest;
 
         @Autowired
-        private OperationScopedBean operationScopedBean;
+        private OperationResponse operationResponse;
 
-        public String getOperationName() {
-            return operationScopedBean.getName();
+        public void handleRequest() {
+            operationResponse.setOutput(String.format("operation response for %s", operationRequest.getInput()));
         }
-    }
-
-    @Retention(RetentionPolicy.RUNTIME)
-    @Scope(OperationScopeImpl.NAME)
-    public @interface OperationScope {
-        @AliasFor(annotation = Scope.class)
-        ScopedProxyMode proxyMode() default ScopedProxyMode.TARGET_CLASS;
     }
 
     public static class OperationScopeImpl implements org.springframework.beans.factory.config.Scope {
-        private final static Logger LOGGER = LoggerFactory.getLogger(OperationScopeImpl.class);
-
-        public final static String NAME = "operation";
+        public final static String SCOPE_NAME = "operation";
 
         @Override
         public Object get(String name, ObjectFactory<?> objectFactory) {
-            OperationContext operationContext = OperationContextHolder.getOperationContext();
-            if(operationContext == null) {
-                throw new RuntimeException();
+            OperationContext operationContext = getOperationContextOrThrow();
+            Object object = operationContext.get(name);
+            if(object == null) {
+                object = objectFactory.getObject();
+                operationContext.set(name, object);
             }
 
-            Object obj = operationContext.getAttribute(name);
-            if(obj == null) {
-                obj = objectFactory.getObject();
-
-                if(obj instanceof OperationScopedBean) {
-                    OperationScopedBean operationScopedBean = (OperationScopedBean)obj;
-                    operationScopedBean.setName(operationContext.name);
-                }
-
-                operationContext.setAttribute(name, obj);
-            }
-
-            return obj;
+            return object;
         }
 
         @Override
         public Object remove(String name) {
-            LOGGER.info("remove() name={}", name);
-
-            OperationContext operationContext = OperationContextHolder.getOperationContext();
-            if(operationContext == null) {
-                throw new RuntimeException();
-            }
-
-            Object obj = operationContext.getAttribute(name);
-            if(obj != null) {
-                operationContext.removeAttribute(name);
-                return obj;
-            }
-
-            return null;
+            throw new UnsupportedOperationException();
         }
 
         @Override
         public void registerDestructionCallback(String name, Runnable callback) {
-            // Optional, intentionally blank
+            getOperationContextOrThrow();
         }
 
         @Override
         public Object resolveContextualObject(String key) {
-            // Optional, intentionally null
+            getOperationContextOrThrow();
             return null;
         }
 
         @Override
         public String getConversationId() {
-            // Optional, intentionally null
+            getOperationContextOrThrow();
             return null;
+        }
+
+        private static OperationContext getOperationContextOrThrow() {
+            OperationContext operationContext = OperationContextHolder.getOperationContext();
+            if(operationContext == null) {
+                throw new IllegalStateException();
+            }
+            return operationContext;
+        }
+    }
+
+    public static class OperationContextHolder {
+        private static OperationContext operationContext;
+
+        public static OperationContext getOperationContext() {
+            return operationContext;
+        }
+
+        public static void setOperationContext(OperationContext operationContext) {
+            OperationContextHolder.operationContext = operationContext;
+        }
+
+        public static void reset() {
+            operationContext = null;
+        }
+    }
+
+    public static class OperationContext {
+        private final Map<String, Object> scopedObjects = new HashMap<>();
+
+        public Object get(String name) {
+            return scopedObjects.get(name);
+        }
+
+        public void set(String name, Object object) {
+            scopedObjects.put(name, object);
+        }
+    }
+
+    public static class OperationRequest {
+        private final String input;
+
+        public OperationRequest(String input) {
+            this.input = input;
+        }
+
+        public String getInput() {
+            return input;
+        }
+    }
+
+    public static class OperationResponse {
+        private String output;
+
+        public String getOutput() {
+            return output;
+        }
+
+        public void setOutput(String output) {
+            this.output = output;
         }
     }
 }
