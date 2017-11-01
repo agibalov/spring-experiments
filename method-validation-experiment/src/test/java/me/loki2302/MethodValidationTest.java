aaf8048444232c44fad8d7e6e887c69a7e1a0ea4
loki2302 <loki2302@loki2302.me>
2017-10-31 22:53:14 -0400

    Added MethodValidationTest

---
+++ method-validation-experiment/src/test/java/me/loki2302/MethodValidationTest.java
@@ -0,0 +1,73 @@
+package me.loki2302;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class MethodValidationTest {
+    @Autowired
+    private MessageProcessor messageProcessor;
+
+    @Test
+    public void shouldThrowIfValidationFails() {
+        Message message = new Message();
+        message.data = null;
+
+        try {
+            messageProcessor.process(message);
+        } catch (ConstraintViolationException e) {
+            Set<ConstraintViolation<?>> violations = e.getConstraintViolations();
+            assertEquals(1, violations.size());
+
+            ConstraintViolation<?> violation = violations.iterator().next();
+            assertEquals("process.message.data", violation.getPropertyPath().toString());
+            assertEquals("must not be empty", violation.getMessage());
+        }
+    }
+
+    @Test
+    public void shouldNotThrowIfValidationDoesNotFail() {
+        Message message = new Message();
+        message.data = "hello";
+        String result = messageProcessor.process(message);
+        assertEquals("HELLO", result);
+    }
+
+    @Configuration
+    @EnableAutoConfiguration
+    public static class Config {
+        @Bean
+        public MessageProcessor messageProcessor() {
+            return new MessageProcessor();
+        }
+    }
+
+    @Validated
+    public static class MessageProcessor {
+        public String process(@Valid Message message) {
+            return message.data.toUpperCase();
+        }
+    }
+
+    public static class Message {
+        @NotEmpty
+        public String data;
+    }
+}
