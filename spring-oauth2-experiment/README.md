aa3286d8c9aafb81187ead49ae665b0c2c78b9a4
agibalov <loki2302@loki2302.me>
2018-09-27 00:25:01 -0400

    Make spring-oauth2-experiment stateless

--- spring-oauth2-experiment/README.md
+++ spring-oauth2-experiment/README.md
@@ -2,4 +2,4 @@
 
 Illustrates how to use `spring-security-oauth2-client` and `spring-security-oauth2-jose` to implement social authentication.
 
-Launch with `./gradlew clean bootRun` and go to `http://localhost:8080`.
+Launch with `./gradlew clean bootRun` and go to `http://localhost:8080`. Click one of GitHub, Facebook and Google, and check the logs.
--- spring-oauth2-experiment/src/main/java/io/agibalov/App.java
+++ spring-oauth2-experiment/src/main/java/io/agibalov/App.java
@@ -1,17 +1,29 @@
 package io.agibalov;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.web.AuthorizationRequestRepository;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashSet;
 
 @SpringBootApplication
 public class App {
@@ -20,13 +32,99 @@ public class App {
     }
 
     @Configuration
+    @Slf4j
     public static class SecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
                     .anyRequest().authenticated()
                     .and()
-                    .oauth2Login();
+                    .oauth2Login()
+                    .authorizationEndpoint()
+                    .authorizationRequestRepository(new CookieAuthorizationRequestRepository())
+                    .and()
+                    .successHandler((request, response, authentication) -> {
+                        log.info("successHandler: {}", authentication);
+                        // TODO: send redirect to /app/entrypoint/{JWT with facebook/google/github token here}
+                        // TODO: the app is responsible for using that JWT to authenticate against this BE
+                    })
+                    .failureHandler((request, response, exception) -> {
+                        log.info("failureHandler", exception);
+                    });
+        }
+
+        /**
+         * disables Servlet sessions
+         */
+        @Bean
+        public ServletContextInitializer servletContextInitializer() {
+            return servletContext -> servletContext.setSessionTrackingModes(new HashSet<>());
+        }
+
+        /**
+         * they only provide one implementation of AuthorizationRequestRepository: HttpSessionOAuth2AuthorizationRequestRepository
+         * It relies on Servlet sessions mechanism (JSESSIONID), which makes this authentication mechanism stateful
+         * CookieAuthorizationRequestRepository is the implementation that relies on cookies instead of sessions
+         */
+        @Slf4j
+        public static class CookieAuthorizationRequestRepository implements AuthorizationRequestRepository<OAuth2AuthorizationRequest> {
+            private final static String COOKIE_NAME = "authorizationRequest";
+            private final static int COOKIE_TTL = 120;
+
+            @SneakyThrows
+            @Override
+            public OAuth2AuthorizationRequest loadAuthorizationRequest(HttpServletRequest request) {
+                Cookie cookie = Arrays.stream(request.getCookies())
+                        .filter(c -> c.getName().equals(COOKIE_NAME))
+                        .findFirst()
+                        .orElse(null);
+                if(cookie == null) {
+                    return null;
+                }
+
+                return deserialize(cookie.getValue());
+            }
+
+            @SneakyThrows
+            @Override
+            public void saveAuthorizationRequest(
+                    OAuth2AuthorizationRequest authorizationRequest,
+                    HttpServletRequest request,
+                    HttpServletResponse response) {
+
+                if(authorizationRequest == null) {
+                    Arrays.stream(request.getCookies())
+                            .filter(c -> c.getName().equals(COOKIE_NAME))
+                            .forEach(c -> {
+                                c.setValue("");
+                                c.setPath("/");
+                                c.setMaxAge(0);
+                                response.addCookie(c);
+                            });
+                    return;
+                }
+
+                Cookie cookie = new Cookie(COOKIE_NAME, serialize(authorizationRequest));
+                cookie.setPath("/");
+                cookie.setHttpOnly(true);
+                cookie.setMaxAge(COOKIE_TTL);
+                response.addCookie(cookie);
+            }
+
+            @Override
+            public OAuth2AuthorizationRequest removeAuthorizationRequest(HttpServletRequest request) {
+                return loadAuthorizationRequest(request);
+            }
+
+            @SneakyThrows
+            private static String serialize(OAuth2AuthorizationRequest o) {
+                return Base64.getEncoder().encodeToString(SerializationUtils.serialize(o));
+            }
+
+            @SneakyThrows
+            private static OAuth2AuthorizationRequest deserialize(String s) {
+                return (OAuth2AuthorizationRequest)SerializationUtils.deserialize(Base64.getDecoder().decode(s));
+            }
         }
     }
 
